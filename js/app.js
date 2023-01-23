22eea3f9d54b6dc88f45cf25e35b498e31be9b1a
Hafees S <hafees15hafi@gmail.com>
2023-01-24 00:27:15 +0530

    Theme updation

--- js/app.js
+++ js/app.js
@@ -8,80 +8,95 @@ const sidebarStyle = getComputedStyle(sidebar);
 const sidebarList = document.getElementById("sidebarList");
 const sidebarListStyle = getComputedStyle(sidebarList);
 
-
-
-// switch theme
-
 const themeDiv = document.querySelector("#theme");
 const themeIcon = document.getElementById("themeIcon");
-var currentColor = 'white';
 var docstyle = document.documentElement.style;
 
-const themeChange = () => {
-    themechanger();
-    function themechanger() {
-        blackorwhite();
-        if (currentColor == 'white') {
-            themeIcon.src = "images/light.png";
-            docstyle.setProperty('--background-color', '#202124');
-            docstyle.setProperty('--text-color', '#e8eaed');
-            docstyle.setProperty('--border', '1px solid #5f6368');
-            docstyle.setProperty('--footer-background', '#303134');
-            menuIcon.src = "images/menubright.png";
-            if (sidebarStyle.visibility == 'visible') {
-                menuIcon.src = "images/closebright.png";
-                if (sidebarListStyle.display == 'none') {
-                    contentRightArrow.src = "images/rightarrowbright.png";
-                } else {
-                    contentRightArrow.src = "images/downarrowbright.png"
-                }
-            } else {
-                menuIcon.src = "images/menubright.png";
-                contentRightArrow.src = "images/rightarrowbright.png";
-            }
-            currentColor = 'black';
+const menuBtn = document.querySelector("#menuBtn");
+const menuBtnId = document.getElementById("menuBtn");
+const menuIcon = document.getElementById("menuIcon");
 
-        } else {
-            docstyle.setProperty('--background-color', '#f8f9fa');
-            themeIcon.src = "images/dark.png";
-            docstyle.setProperty('--text-color', 'black');
-            docstyle.setProperty('--border', '1px solid #dadce0');
-            docstyle.setProperty('--footer-background', '#f8f9fa');
-            menuIcon.src = "images/iconmenu.png";
-            if (sidebarStyle.visibility == 'visible') {
-                menuIcon.src = "images/close.png";
-                if (sidebarListStyle.display == 'none') {
-                    contentRightArrow.src = "images/rightarrow.png";
-                } else {
-                    contentRightArrow.src = "images/downarrow.png"
-                }
-            } else {
-                menuIcon.src = "images/iconmenu.png";
-                contentRightArrow.src="images/rightarrow.png";
-            }
-            currentColor = 'white';
-        }
-    }
 
+var whiteTheme = () => {
+    docstyle.setProperty('--background-color', '#f8f9fa');
+    themeIcon.src = "images/dark.png";
+    docstyle.setProperty('--text-color', 'black');
+    docstyle.setProperty('--border', '1px solid #dadce0');
+    docstyle.setProperty('--footer-background', '#f8f9fa');
+    menuIcon.src = "images/iconmenu.png";
+    contentQuery.style.color = "black";
 }
+var blackTheme = () => {
+    themeIcon.src = "images/light.png";
+    docstyle.setProperty('--background-color', '#202124');
+    docstyle.setProperty('--text-color', '#e8eaed');
+    docstyle.setProperty('--border', '1px solid #5f6368');
+    docstyle.setProperty('--footer-background', '#303134');
+    menuIcon.src = "images/menubright.png";
+    contentQuery.style.color = "white"
+}
+
+var currentTheme = () => {
+    var theme = localStorage.getItem("theme")
+    if (theme) {
+        return theme;
+    } else {
+        return "white";
+    }
+};
+
+var loadCurrentTheme = () => {
+    var theme = localStorage.getItem("theme");
+    if (theme == "white") {
+        whiteTheme();
+    } else if (theme == "black") {
+        blackTheme();
+    }
+};
+
+loadCurrentTheme();
+
+// For theme changer
+var currentColor=currentTheme();
+
+// switch theme
+const themeChange = () => {
+    localStorage.clear();
+    if (currentColor == "white") {
+        blackTheme();
+        localStorage.setItem("theme", "black");
+        if (sidebarStyle.visibility == 'visible') {
+            menuIcon.src = "images/closebright.png";
+        } else {
+            menuIcon.src = "images/menubright.png";
+        }
+        currentColor="black";
+    } else {
+        whiteTheme();
+        localStorage.setItem("theme", "white");
+
+        if (sidebarStyle.visibility == 'visible') {
+            menuIcon.src = "images/close.png";
+        } else {
+            menuIcon.src = "images/iconmenu.png";
+        }
+        currentColor="white"
+    }
+}
+
 themeDiv.addEventListener('click', themeChange);
 // 
 
 
 // menu button handler
 
-const menuBtn = document.querySelector("#menuBtn");
-const menuBtnId = document.getElementById("menuBtn");
 
-
-
-const menuIcon = document.getElementById("menuIcon");
 
 const menuHandler = () => {
     if (sidebarStyle.visibility == 'hidden') {
         sidebar.style.visibility = 'visible';
         sidebar.style.display = 'flex';
-        if (currentColor == 'white') {
+        if (currentColor == "white") {
             menuIcon.src = "images/close.png";
         } else {
             menuIcon.src = "images/closebright.png";
@@ -90,7 +105,7 @@ const menuHandler = () => {
         sidebar.style.visibility = 'hidden';
         sidebar.style.display = 'none';
         menuIcon.src = "images/iconmenu.png";
-        if (currentColor == 'white') {
+        if (currentColor == "white") {
             menuIcon.src = "images/iconmenu.png";
         } else {
             menuIcon.src = "images/menubright.png";
@@ -100,42 +115,48 @@ const menuHandler = () => {
 menuBtn.addEventListener('click', menuHandler);
 
 // dark or white theme
-function blackorwhite() {
-    const headerStyle = getComputedStyle(document.getElementById("header"));
-    if (headerStyle.backgroundColor == 'rgb(32, 33, 36)') {
-        currentColor = 'black';
-        return currentColor;
-    } else {
-        currentColor = 'white';
-        return currentColor;
-    }
-}
+
 
 
 // content listing
 const contentDown = () => {
     contentLister();
     function contentLister() {
-        blackorwhite();
         if (sidebarListStyle.display == 'none') {
             sidebarList.style.display = 'flex';
             sidebarList.style.visibility = 'visible';
-            if (currentColor == 'white') {
-                contentRightArrow.src = "images/downarrow.png";
-            } else {
-                contentRightArrow.src = "images/downarrowbright.png";
-            }
+            rotateArrow("forw");
         } else if (sidebarListStyle.display == 'flex') {
             sidebarList.style.visibility = 'hidden';
             sidebarList.style.display = 'none';
-            if (currentColor == 'white') {
-                contentRightArrow.src = "images/rightarrow.png";
-            } else {
-                contentRightArrow.src = "images/rightarrowbright.png";
-            }
+            rotateArrow("backw")
         }
     }
 }
 contentQuery.addEventListener('click', contentDown);
 
-// 
+// Rotateicons
+
+const rotateArrow = (params) => {
+    const rotateArrow = document.querySelector('#contentsRight');
+    if (params == "forw") {
+        rotateArrow.style.transform = 'rotate(0deg)';
+        console.log("rotated")
+    } else {
+        rotateArrow.style.transform = 'rotate(-90deg)';
+        console.log("rotatedback")
+
+    }
+}
+
+//scroll function
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-10%";
+  }
+  prevScrollpos = currentScrollPos;
+}
